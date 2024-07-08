1a356d1035ad0f534fa01014029c86d776535508
Eric Pouliquen <epouliquen@ls-vr.com>
2024-07-08 15:09:17 +0200

    useRootEngine tests but fail on ports added using flatmap

--- src/GraphDebug.jsx
+++ src/GraphDebug.jsx
@@ -8,7 +8,8 @@ import { subscribe } from "valtio";
 const GraphDebug = ({ nodes }) => {
   const [, forceUpdate] = useReducer((x) => x + 1, 0);
   const { ...inputs } = useRootEngine(nodes, engine);
-  console.log("rootNode inputs", inputs);
+  console.log("rootNode inputs", inputs.ids);
+  inputs.ids && inputs.ids.entriesControl.map((val, index) => console.log(inputs["triggerGLBVisibility" + index]));
 
   subscribe(attributes, () => forceUpdate());
 
@@ -16,6 +17,7 @@ const GraphDebug = ({ nodes }) => {
     <div className="graphDebug">
       <h1>GRAPH DEBUG</h1>
       <UI />
+      {inputs.ids && inputs.ids.entriesControl.map((val, index) => inputs["triggerGLBVisibility" + index] && <p key={index}>{inputs["glbURL" + index].string}</p>)}
       {/* {triggerGLBVisibility1 && <p>{glbURL.string}</p>} */}
     </div>
   );
